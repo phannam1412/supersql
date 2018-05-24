45bcd5e44d52e48eadb3802b7b6f1284aa46d1fa
nam.phanhoai <nam.phanhoai@lazada.com>
2018-05-24 16:46:12 +0700

    updated

--- wordpress/playground.php
+++ wordpress/playground.php
@@ -13,7 +13,7 @@ require_once( dirname(__FILE__) . '/wp-load.php' );
 // Set up the WordPress query.
 wp();
 
-SuperSql::defineSelectFromTable("users", function() {
+SuperSql::defineSelectFromTable("user", function() {
     $users = get_users();
     $users = array_map(function($item){
         return $item->data;
@@ -22,11 +22,25 @@ SuperSql::defineSelectFromTable("users", function() {
     return $data;
 });
 
-SuperSql::defineSelectFromTable("posts", function() {
-    $posts = get_posts();
+SuperSql::defineSelectFromTable("pending_post", function() {
+    $posts = get_posts(['numberposts' => 1000, 'post_status' => 'pending']);
     $data = json_decode(json_encode($posts), TRUE);
     return $data;
 });
 
-$rows = SuperSql::execute("SELECT * FROM posts");
-SuperSql::printRows($rows);
+
+SuperSql::defineSelectFromTable("post", function() {
+    $posts = get_posts(['numberposts' => 1000]);
+    $data = json_decode(json_encode($posts), TRUE);
+    return $data;
+});
+
+try {
+    if(count($argv) >= 2)
+        $rows = SuperSql::execute($argv[1]);
+    else
+        $rows = SuperSql::execute("SELECT user_email, post_title FROM pending_post p inner join user u on p.post_author = u.ID");
+    SuperSql::printRows($rows);
+} catch(Exception $e) {
+    print $e->getMessage() . PHP_EOL;
+}
